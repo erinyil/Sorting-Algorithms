d7167614fe7cb28645ffa08ae89effc0cbc15e3b
Erin Yilmaz <64546500+erinyil@users.noreply.github.com>
2020-07-03 22:39:17 -0700

    Create sortAlgs.h

---
+++ sortAlgs.h
@@ -0,0 +1,25 @@
+/**********************************************************
+* Erin Yilmaz
+* Homework 5
+* Comp 15
+* sortAlgs.h
+*
+* sortAlgs.h contains the declarations for 3 functions for
+* my initial sorting algorithm function calls. 
+*********************************************************/
+
+#ifndef _SORTALGS_H_
+#define _SORTALGS_H_
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(vector<int> &numbers);
+
+void radixSort(vector<int> &numbers);
+
+void shellSort(vector<int> &numbers);
+
+#endif
